5584330910f3caab53b3d8895d68578c523ec7b4
netinhobabinski <159185041+netinhobabinski@users.noreply.github.com>
2025-03-25 11:19:00 -0300

    25

---
+++ img/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function(){
+
+    const aumentarFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuirFonteBotao = document.getElementById('diminuir-fonte');
+
+let tamanhoAtualFonte = 1 ;
+
+ aumentarFonteBotao.addEventListener('click', function(){
+ tamanhoAtualFonte +=0;1;
+ document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+ 
+})
+diminuirFonteBotao.addEventListener('click', function(){
+    tamanhoAtualFonte +=0;1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`   
+
+
+
+})
+
+})
